ccb1816618c12f790f701548db5deda39f854418
joebot997 <100249543+joebot997@users.noreply.github.com>
2024-09-30 11:53:59 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
+import netlify from '@astrojs/netlify/functions'; // Import the Netlify adapter
 import sitemap from "@inox-tools/sitemap-ext";
 
+// Flag for determining if the build is for production or development
 const prodBuild = true;
 
+// Set the site URL based on the build environment
 const site = prodBuild ? "https://aluu.xyz" : "http://localhost:3000";
 
 export default defineConfig({
@@ -13,7 +16,8 @@ export default defineConfig({
       lastmod: new Date(),
     }),
   ],
-  output: "hybrid", // Change from 'server' to 'static'
+  output: "hybrid", // Keep 'hybrid' if you need both static and SSR rendering or change to 'static' or 'server'
+  adapter: netlify(), // Use the Netlify adapter
   vite: {
     server: {
       watch: {
